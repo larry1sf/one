d9159f7108781f4011eafd26300b2f05d34d62b9
larry <larryycel@gmail.com>
2024-04-27 23:05:54 -0500

    int

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+// paginas.
+import Inicio from './pages/Inicio';
+
+
+// css.
+import './css/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Inicio />
   </React.StrictMode>,
-)
+);
---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react';
+
+function Inicio() {
+
+  return (
+    <>
+      <Home></Home>
+    </>
+  );
+}
+
+
+
+// page inicial.
+function Home() {
+  return (
+    <>
+      home.
+    </>
+  );
+}
+
+
+
+export default Inicio;
